f28cdde9c8321de59f3cf50191df361b3e835440
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2021-11-25 18:17:25 -0300

    adjustment on routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { createUserController } from "./usecases/createUser";
 
 const router = Router();
 
 router.get("/", (request, response) => {
-  return response.status(201).send("Route working fine");
+  // return response.status(201).send("Route working fine");
+  return createUserController.handle(request, response);
 });
 
 export { router };
