513e2a90c2fad664d586740a029972dc7572c618
Mateus Pedroso <mateuspsilva.dev@gmail.com>
2021-11-25 16:51:52 -0300

    added the send email function on the createuser usecase

--- src/usecases/createUser/CreateUserUseCase.ts
+++ src/usecases/createUser/CreateUserUseCase.ts
@@ -1,9 +1,13 @@
 import { User } from "../../entities/User";
+import { IMailProvider } from "../../providers/IMailProvider";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 import { ICreateUserRequestDTO } from "./CreateUserDTO";
 
 export class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(
+    private usersRepository: IUsersRepository,
+    private mailProvider: IMailProvider
+  ) {}
 
   async execute(data: ICreateUserRequestDTO) {
     const userAlreadyExists = this.usersRepository.findByEmail(data.email);
@@ -12,5 +16,17 @@ export class CreateUserUseCase {
     }
     const user = new User(data);
     await this.usersRepository.save(user);
+    this.mailProvider.sendMail({
+      to: {
+        email: data.email,
+        name: data.name,
+      },
+      from: {
+        email: "test@test.com.br",
+        name: "test sender",
+      },
+      subject: "subject test",
+      body: `<p>A body test for an email.</p>`,
+    });
   }
 }
